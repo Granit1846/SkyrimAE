8264140f31a4c61aad88e45e1544a0e41e517678
Skeorz <Granit1846@gmail.com>
2026-01-19 15:02:23 +0300

    Update 2026-01-19 15:02:23

--- TDL_StreamPlugin/src/tools/tdl_send.cpp
+++ TDL_StreamPlugin/src/tools/tdl_send.cpp
@@ -2,7 +2,6 @@
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <Windows.h>
-#include <shellapi.h>
 
 #include <string>
 #include <cwchar>
@@ -63,23 +62,18 @@ static int SendLineToPipeUtf8(const std::string& lineUtf8)
 }
 
 // Windows subsystem entry point => no console window
-int WINAPI wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
+int wmain(int argc, wchar_t* argv[])
 {
-	int argc = 0;
-	LPWSTR* argv = CommandLineToArgvW(GetCommandLineW(), &argc);
-	if (!argv || argc < 2) {
-		if (argv) LocalFree(argv);
+	if (argc < 2) {
 		return 2;  // usage
 	}
 
 	std::wstring mode = argv[1];
-
 	std::string line;
 
 	// tdl_send.exe NORMAL ACTION [SOURCE]
 	if (EqualsNoCase(mode, L"NORMAL")) {
 		if (argc < 3) {
-			LocalFree(argv);
 			return 2;
 		}
 
@@ -92,28 +86,23 @@ int WINAPI wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
 		source = ClampSource(source);
 
 		line = action + "|" + std::to_string(source);
-		LocalFree(argv);
 		return SendLineToPipeUtf8(line);
 	}
 
 	// tdl_send.exe FORCE1 ACTION
 	if (EqualsNoCase(mode, L"FORCE1")) {
 		if (argc < 3) {
-			LocalFree(argv);
 			return 2;
 		}
 
 		std::string action = WideToUtf8(argv[2]);
 		line = "FORCE1|" + action;
-
-		LocalFree(argv);
 		return SendLineToPipeUtf8(line);
 	}
 
 	// tdl_send.exe FORCE ACTION COUNT [INTERVAL]
 	if (EqualsNoCase(mode, L"FORCE")) {
 		if (argc < 4) {
-			LocalFree(argv);
 			return 2;
 		}
 
@@ -121,20 +110,15 @@ int WINAPI wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
 		int count = (int)wcstol(argv[3], nullptr, 10);
 
 		if (argc >= 5) {
-			// interval provided
 			double interval = wcstod(argv[4], nullptr);
-			// Форматируем без лишней экзотики: как есть
 			line = "FORCE|" + action + "|" + std::to_string(count) + "|" + std::to_string(interval);
 		}
 		else {
 			line = "FORCE|" + action + "|" + std::to_string(count);
 		}
 
-		LocalFree(argv);
 		return SendLineToPipeUtf8(line);
 	}
 
-	// Unknown mode
-	LocalFree(argv);
 	return 2;
 }
